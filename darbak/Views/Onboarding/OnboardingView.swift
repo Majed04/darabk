ae60b5b462fbf0c79ca177c9517f5fa9fca6543a
Ali Alsuwaiyel <ali@10-101-52-152.0200.twq.sa.iosda.org>
2025-08-04 09:16:28 +0300

    made the onboarding page

--- darbak/Views/Onboarding/OnboardingView.swift
+++ darbak/Views/Onboarding/OnboardingView.swift
@@ -1,5 +1,5 @@
 //
-//  Welcome.swift
+//  OnboardingView.swift
 //  darbak
 //
 //  Created by Majed on 05/02/1447 AH.
@@ -10,33 +10,64 @@ import Lottie
 
 struct Onboarding: View {
     @EnvironmentObject var user: User
-
-
-        var body: some View {
+    @State private var navigateToChallenge = false
+    
+    var body: some View {
         VStack(alignment: .center, spacing: 0) {
+            // Main Image
             Image("onboardingPicture")
                 .resizable()
                 .aspectRatio(contentMode: .fit)
                 .frame(width: 630, height: 630)
+                .padd
+                .clipShape(
+                    .rect(
+                        bottomLeadingRadius: 180,
+                        bottomTrailingRadius: 180
+                    )
+                )
+                .overlay(
+                    UnevenRoundedRectangle(
+                        bottomLeadingRadius: 180,
+                        bottomTrailingRadius: 180
+                    )
+                    .stroke(Color.black, lineWidth: 6)
+                )
             
+            // Title Text
             Text("المشي صار فعالية")
                 .padding(.top, 30)
                 .font(.system(size: 35, weight: .bold))
                 .foregroundColor(Color(hex: "#1B5299"))
-            Spacer()
-                
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .ignoresSafeArea(.all, edges: .top)
             
+            Spacer()
+            
+            // Action Button
+            CustomButton(title: "عرفنا عليك") {
+                navigateToChallenge = false
+            }
+            .padding(.horizontal, 24)
+            .padding(.top, 50)
+            .padding(.bottom, 50)
+        }
+        .fontDesign(.rounded)
+        .background(Color.white)
+        .clipShape(
+            .rect(
+                bottomLeadingRadius: 30,
+                bottomTrailingRadius: 30
+            )
+        )
+        .ignoresSafeArea(.all, edges: .top)
+        .navigationDestination(isPresented: $navigateToChallenge) {
+            Challenge()
+        }
     }
-    
 }
-
 
 #Preview {
-    Onboarding()
-    
+    NavigationStack {
+        Onboarding()
+            .environmentObject(User())
+    }
 }
-
-
